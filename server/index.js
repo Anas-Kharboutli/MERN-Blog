7da39cb1bcb9e2a36e223a990ddbdb0d08d1bfdc
Anas-Kharboutli <anas.kharboutli@gmail.com>
2024-05-08 18:15:34 +0200

    Connected to Mongodb

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
+import { error } from "console";
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+.then(() => console.log("MongoDB is Connected !"))
+.catch((error) => { console.log(error)});
 
 const app = express();
 
 const port = 3000;
-
 app.listen(port, console.log(`listening on port: ${port} !`));
