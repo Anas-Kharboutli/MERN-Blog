639e7b0cbcc13844fa8f31dd94391aea1210555e
Anas-Kharboutli <anas.kharboutli@gmail.com>
2024-05-08 17:31:06 +0200

    created server with express and connected.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Home, About, Dashboard, SignIn, SignUp, Projects } from "./pages/exports";
+import { Home, About, Dashboard, SignIn, SignUp, Projects, Header } from "./pages/exports";
 
 const App = () => {
   return (
     <BrowserRouter>
+
+    <Header />
+    
     <Routes>
 
       <Route path='/'          element={<Home />} />
--- client/src/pages/exports.js
+++ client/src/pages/exports.js
@@ -4,6 +4,7 @@ import Dashboard from "./Dashboard";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
 import Projects from "./Projects";
+import Header from "../components/Header";
 
 
-export { Home, About, Dashboard, SignIn, SignUp, Projects };
+export { Home, About, Dashboard, SignIn, SignUp, Projects, Header };
