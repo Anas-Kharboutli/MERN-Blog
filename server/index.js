963279438f571ac23affbeea0ef970c2b0c0566b
Anas-Kharboutli <anas.kharboutli@gmail.com>
2024-05-09 14:50:13 +0200

    sign-up backend is done

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import { error } from "console";
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
+import authRoute from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -10,6 +12,21 @@ mongoose.connect(process.env.MONGO)
 .catch((error) => { console.log(error)});
 
 const app = express();
+app.use(express.json());
 
 const port = 3000;
 app.listen(port, console.log(`listening on port: ${port} !`));
+
+app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    res.status(statusCode).json(
+        {
+            success: false,
+            statusCode, 
+            message
+        });
+});
